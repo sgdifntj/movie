e120b2dbd3cc16401f8af865302bfba17b1dbd25
sujeetjha8 <devill7879@gmail.com>
2024-08-12 18:25:05 +0530

    gf

---
+++ app.js
@@ -0,0 +1,13 @@
+
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle");
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
